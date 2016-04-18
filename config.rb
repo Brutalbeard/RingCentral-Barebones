17d389deae346d831fe2d18cbe07854ea78449af
Brutalbeard <jceloria@gmail.com>
2016-04-18 10:23:43 -0600

    Delete config.rb

--- config.rb
+++
@@ -1,6 +0,0 @@
-$RC_APP_KEY="gFpowr2qRTmOXI3cVitQvA"
-$RC_APP_SECRET="yqTplij8RWq2r4Qe9zASzQZBNVITPrReOhmYcJwYFPTw"
-$RC_SERVER_SANDBOX = "https://platform.devtest.ringcentral.com"
-#RC_APP_REDIRECT_URL=myRcApiRedirectUrl
-$userName = "+18024486684"
-$password = "newGuy37"
